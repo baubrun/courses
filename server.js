acd52113428669f692d57c095486767441950b3f
baubrun <baubelf@gmail.com>
2020-11-23 19:54:36 -0500

    updated pck json

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
-import compress from "compression"
-import helmet from "helmet"
 import userRoutes from "./routes/user.js"
 import authRoutes from "./routes/auth.js"
 import courseRoutes from "./routes/course.js"
@@ -29,8 +27,8 @@ app.use(express.json({
 app.use(express.urlencoded({
     extended: true
 }))
-app.use(compress())
-app.use(helmet())
+
+
 app.use(cors())
 
 app.use((error, req, res, next) => {
